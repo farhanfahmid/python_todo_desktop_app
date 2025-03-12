738ff237a984a429124a73cbe48a713cc620e04b
farhanfahmid <fahmidfarhan274@gmail.com>
2025-03-12 23:28:52 +0600

    Fix IndexError of Edit and Remove buttons by implementing popup warning boxes

--- gui.py
+++ gui.py
@@ -3,8 +3,10 @@ from contextlib import nullcontext
 import functions
 
 import FreeSimpleGUI as sg
-
-label = sg.Text("Type in a To-Do Item")
+import time
+now = time.strftime("%b %d, %Y   %H:%M:%S")
+label = sg.Text("What To Do Today?")
+date = sg.Text(now)
 
 input_box = sg.InputText(key="todo")
 
@@ -12,6 +14,7 @@ input_box = sg.InputText(key="todo")
 add_button = sg.Button("Add")
 edit_button = sg.Button("Edit")
 remove_button = sg.Button("Remove")
+exit_button = sg.Button("Exit")
 
 #define a list box that gets the added todo items and lists them
 list_box = sg.Listbox(values = functions.get_todos(), key='todo_item',
@@ -19,10 +22,11 @@ list_box = sg.Listbox(values = functions.get_todos(), key='todo_item',
 
 #create a window with the defined elements
 window = sg.Window('My To-Do App',
-                   layout=[[label],
+                   layout=[[date],
+                            [label],
                            [input_box, add_button],
-                           [list_box, edit_button],
-                           [remove_button]],
+                           [list_box, edit_button, remove_button],
+                           [exit_button]],
 
                    font=('Helvetica', 16))
 
@@ -41,29 +45,36 @@ while True:
             window['todo'].update(value='')
 
         case 'Edit':
-            todo_to_edit = values['todo_item'][0]
-            new_todo = values['todo']
+            try:
+                todo_to_edit = values['todo_item'][0]
+                new_todo = values['todo']
 
-            todos = functions.get_todos() #get current todos
-            index = todos.index(todo_to_edit) #get index of the todo to replace
-            todos[index] = new_todo.title() #replace existing todo with new todo
-            functions.write_todos(todos) #write new updated list
-            window['todo_item'].update(values=todos) #give the instance of updated to-do list in real time and update values with new todos
-            window['todo'].update(value='')
+                todos = functions.get_todos() #get current todos
+                index = todos.index(todo_to_edit) #get index of the todo to replace
+                todos[index] = new_todo.title() #replace existing todo with new todo
+                functions.write_todos(todos) #write new updated list
+                window['todo_item'].update(values=todos) #give the instance of updated to-do list in real time and update values with new todos
+                window['todo'].update(value='')
+
+            except IndexError:
+                sg.popup("Please select an item first to edit")
 
         case 'todo_item':
             window['todo'].update(value = values['todo_item'][0]) #when clicking on a todo item, make the input box's todo text into that clicked to_do item
 
         case 'Remove':
-            todo_to_remove = values['todo_item'][0]
-            index = todos.index(todo_to_remove)
-            todos.pop(index)
-            functions.write_todos(todos)
-            window['todo_item'].update(values=todos)
-            window['todo'].update(value='')
 
-
+            try:
+                todo_to_remove = values['todo_item'][0]
+                index = todos.index(todo_to_remove)
+                todos.pop(index)
+                functions.write_todos(todos)
+                window['todo_item'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Please select an item to remove")
 
         case sg.WINDOW_CLOSED: break
+        case 'Exit': break
 
 window.close()
